b16cd7b295af9a6cbf450076f8005f393bde8188
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-21 20:34:28 +0600

    for lop

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const numbers = [1, 21, 56];
-const friends = ["mango", "kola", "jam"];
-const address = ["dhaka", "comilla", "gazipur"];
-const mixedArray = [12, "dhaka", true, 56.36];
-
-numbers.push(989, 12, 23);
-numbers.pop();
-
-// for(let num of numbers){
-//     console.log(num);
-//     console.log("o ok alright");
+let num = 0;
+// while(num < 5){
+//     console.log(num)
+//     num++
 // }
 
-let num = 0;
-let sum = 0;
-
-while (num <= 50) {
-  console.log("LOOP", num);
-  sum = sum + num;
-  console.log(sum);
-  num = num + 25;
+for(let num = 0 ; num < 5 ; num++){
+console.log(num);
 }
