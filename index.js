ccc3a21e1b19e34299a453ed0c362f39265e58a0
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-21 17:03:11 +0600

    javascript add

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const numbers = [ 1 , 21, 56];
+const friends = [ 'mango', 'kola', 'jam'];
+const adress =['dhaka', 'comilla', 'gazipur']
+const mixedArray = [12, 'dhaka', true, 56.36]
+console.log(mixedArray[3]);
