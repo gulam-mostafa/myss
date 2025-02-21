5e4f4f8ee29bfd1d9561ed042b287e39e0806d4d
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-21 20:05:23 +0600

    while loop

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 const numbers = [ 1 , 21, 56];
 const friends = [ 'mango', 'kola', 'jam'];
-const adress =['dhaka', 'comilla', 'gazipur']
+const address =['dhaka', 'comilla', 'gazipur']
 const mixedArray = [12, 'dhaka', true, 56.36]
-console.log(mixedArray[3]);
+
+numbers.push(989, 12, 23)
+numbers.pop()
+
+
+// for(let num of numbers){
+//     console.log(num);
+//     console.log("o ok alright");
+// }
+
+let num = 0;
+
+while(num <= 50 ){
+    console.log("LOOP" , num);
+ 
+    num = num +5 ;
+}
