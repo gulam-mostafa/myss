ebcdb1484c3cf6d62861b29bc250572e0b9cfd5f
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-21 20:30:23 +0600

    whele loop finis

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+const numbers = [1, 21, 56];
+const friends = ["mango", "kola", "jam"];
+const address = ["dhaka", "comilla", "gazipur"];
+const mixedArray = [12, "dhaka", true, 56.36];
 
-const numbers = [ 1 , 21, 56];
-const friends = [ 'mango', 'kola', 'jam'];
-const address =['dhaka', 'comilla', 'gazipur']
-const mixedArray = [12, 'dhaka', true, 56.36]
-
-numbers.push(989, 12, 23)
-numbers.pop()
-
+numbers.push(989, 12, 23);
+numbers.pop();
 
 // for(let num of numbers){
 //     console.log(num);
@@ -14,9 +12,11 @@ numbers.pop()
 // }
 
 let num = 0;
+let sum = 0;
 
-while(num <= 50 ){
-    console.log("LOOP" , num);
- 
-    num = num +5 ;
+while (num <= 50) {
+  console.log("LOOP", num);
+  sum = sum + num;
+  console.log(sum);
+  num = num + 25;
 }
