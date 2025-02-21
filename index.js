da976adcdf0ba1b3d08ec2ab0cba7193a66d30dc
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-21 20:59:03 +0600

    even % odd number

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let num = 0;
+let num = 2;
 // while(num < 5){
 //     console.log(num)
 //     num++
 // }
 
-for(let num = 0 ; num < 5 ; num++){
-console.log(num);
+for(let num = 0 ; num < 50 ; num++){
+if(num%2 !==0){
+    console.log(num);
 }
+}
+for(let num = 0 ; num <= 50 ; num++){
+    if(num%2 ===0){
+        console.log(num);
+    }
+    }
