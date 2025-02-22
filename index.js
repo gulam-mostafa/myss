43772f6ec19b24363af2b1618ee10e104e3cf363
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-22 10:20:33 +0600

    split or string divided also slice

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let city = "dhaka";
 let town = "Dhaka";
-let capital= "gazipur Dhaka "
-
-let cityLowerCae = city.toLowerCase();
-let townCapital = town.toUpperCase();
-let cpitalSpaceRemove = capital.trim().toLowerCase();
-console.log(cityLowerCae, townCapital, cpitalSpaceRemove);
+let capital = "gazipur Dhaka ";
+let capitalSplit = capital.split(" ")
+let cityLowerCae = city.slice(0, 3);
+console.log(capitalSplit);
+//let townCapital = town.toUpperCase();
+//let cpitalSpaceRemove = capital.trim().toLowerCase();
+//console.log(cityLowerCae, townCapital, cpitalSpaceRemove);
