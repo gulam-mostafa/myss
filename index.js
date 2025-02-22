2eb70ce3a5acf5ab76e9241de4d6be7ed17ea8f0
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-22 14:49:13 +0600

    for loop array

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ let keys =Object.keys(mobile);
 let valus = Object.values(mobile);
 // console.log(keys , valus);
 for( let prop in mobile){
-    console.log(prop);
-    console.log(mobile[prop]);
+    // console.log(prop);
+    // console.log(mobile[prop]);
 }
+
+let numbers = [23, 256, 245, 455, 45,785, 426, 78,]
+for(i = 0; i < numbers.length; i = i+1){
+    console.log(numbers[i]);
+}
+console.log('result ', numbers.length);
