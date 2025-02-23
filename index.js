d04c016006fcb50d7e08c734490007b9d74f28e2
gulam-mostafa <neamulhoque33@gmail.com>
2025-02-23 09:36:46 +0600

    fungtin

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-let mobile ={
-    brand:'reame',
-    price: 2100,
-    color: 'black',
-    isClean: false
-}
-mobile.brand= 'realme-x2'
-// console.log(mobile);
+let price = 45;
 
-let keys =Object.keys(mobile);
-let valus = Object.values(mobile);
-// console.log(keys , valus);
-for( let prop in mobile){
-    // console.log(prop);
-    // console.log(mobile[prop]);
+
+squar(6)
+
+function squar(number){
+console.log(number * number);
+
 }
 
-let numbers = [23, 256, 245, 455, 45,785, 426, 78,]
-for(i = 0; i < numbers.length; i = i+1){
-    console.log(numbers[i]);
-}
-console.log('result ', numbers.length);
